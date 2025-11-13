5a40f83e0fadb0ff07e9f48bb0f9c0f6cd0f6017
maybites <registered@maybites.ch>
2025-11-13 13:20:56 +0100

    removed obsolete classes and attributes

--- src/haywire/core/types/ports.py
+++ src/haywire/core/types/ports.py
@@ -23,14 +23,12 @@ class DataPort(DataPortIdentity):
         
     # Runtime fields (not in identity)
     data: Optional[DataField] = None
-    metadata: dict = field(default_factory=dict)
     
     # Pin-specific (unused for Config/Property)
     is_connected: bool = False
     
     # Inlet-specific (unused for Outlet/Config/Property)
     is_pooled: bool = False
-    callback: Optional[Callable] = None
     use_mode: str = 'optional'
     is_lazy: bool = False
     
@@ -118,10 +116,6 @@ class PortInlet(DataPort):
         if self.data is None and self.flow_type == FlowType.DATA:
             self.data = DataFieldFactory.create(self, is_pooled=self.is_pooled)
 
-        # Auto-trigger callback on value change
-        if self.callback and self.data:
-            self.data.add_observer(lambda v: self.callback() if self.callback else None)
-
 @dataclass
 class PortOutlet(DataPort):
     def __post_init__(self):
@@ -130,167 +124,3 @@ class PortOutlet(DataPort):
         if self.data is None and self.flow_type == FlowType.DATA:
             self.data = DataFieldFactory.create(self, is_pooled=False)
 
-
-@DeprecationWarning
-@dataclass
-class PinSpec:
-    """Specification for creating any configurable element"""
-    flow_type: FlowType
-    data_spec: Optional[DataPortIdentity] = None
-    label: str = ''
-    widget: Optional[str] = None
-    ui: dict = None
-    is_pooled: bool = False
-    callback: Optional[Callable] = None
-    is_config: bool = False
-    is_property: bool = False
-    is_outlet: bool = False
-    
-    def create_inlet(self, element_id: str, node_instance=None) -> PortInlet:
-        """Instantiate an Inlet from this spec"""
-        element = PortInlet(
-            id=element_id,
-            spec=self.data_spec,
-            label=self.label or element_id.replace('_', ' ').title(),
-            flow_type=self.flow_type,
-            is_pooled=self.is_pooled,
-            widget=self.widget,
-            ui=self.ui or {},
-            callback=None,  # Set up callback below
-            data=DataFieldFactory.create(self.data_spec,is_pooled=self.is_pooled) if self.data_spec else None
-        )
-        
-        # Set up callback for configs
-        if self.callback and node_instance:
-            # Bind callback to node instance
-            bound_callback = lambda: self.callback(node_instance)
-            element.callback = bound_callback
-            
-            # Attach observer to data field
-            if element.data:
-                element.data.add_observer(lambda v: bound_callback())
-        
-        return element
-
-    def create_outlet(self, element_id: str) -> PortOutlet:
-        """Instantiate an Outlet from this spec"""
-        return PortOutlet(
-            id=element_id,
-            spec=self.data_spec,
-            label=self.label or element_id.replace('_', ' ').title(),
-            flow_type=self.flow_type,
-            data=DataFieldFactory.create(self.data_spec,is_pooled=False) if self.data_spec else None
-        )
-    
-@DeprecationWarning
-class PortBuilder:
-    """Factory for creating PinSpecs"""
-
-    @staticmethod
-    def config(spec: DataPortIdentity, label: str = '', callback=None, **kwargs) -> PinSpec:
-        """
-        Config - has callback, no pin visible
-        
-        Usage:
-            PinBuilder.config(FLOAT, label='Value', callback=on_change)
-            PinBuilder.config(FLOAT(label='Value'), callback=on_change)
-        """
-        # If label provided as kwarg, override spec
-        if label:
-            spec = spec(label=label)
-        
-        return PinSpec(
-            flow_type=FlowType.NONE,
-            data_spec=spec,
-            label=spec.label,
-            callback=callback,
-            is_config=True,
-            **kwargs
-        )
-    
-    @staticmethod
-    def property(spec: DataPortIdentity, label: str = '', **kwargs) -> PinSpec:
-        """
-        Property - no callback, no pin visible
-        
-        Usage:
-            PinBuilder.property(FLOAT, label='Value')
-            PinBuilder.property(FLOAT(label='Value'))
-        """
-        # If label provided as kwarg, override spec
-        if label:
-            spec = spec(label=label)
-        
-        return PinSpec(
-            flow_type=FlowType.NONE,
-            data_spec=spec,
-            label=spec.label,
-            is_property=True,
-            **kwargs
-        )
-        
-    @staticmethod
-    def inlet(spec: DataPortIdentity, label: str = '', **kwargs) -> PinSpec:
-        """
-        Data inlet pin
-        
-        Usage:
-            PinBuilder.inlet(FLOAT, label='Radius')
-            PinBuilder.inlet(FLOAT(label='Radius', value=10.0))
-        """
-        # If label provided as kwarg, override spec
-        if label:
-            spec = spec(label=label)
-        
-        return PinSpec(
-            flow_type=FlowType.DATA,
-            data_spec=spec,
-            label=spec.label,
-            **kwargs
-        )
-    
-    @staticmethod
-    def ctrl_inlet(label: str = '', **kwargs) -> PinSpec:
-        return PinSpec(
-            flow_type=FlowType.CTRL,
-            label=label,
-            **kwargs
-        )
-    
-    @staticmethod
-    def callback_inlet(label: str = '', **kwargs) -> PinSpec:
-        return PinSpec(
-            flow_type=FlowType.CALLBACK,
-            label=label,
-            **kwargs
-        )
-    
-    @staticmethod
-    def outlet(spec: DataPortIdentity, label: str = '', **kwargs) -> PinSpec:
-        """
-        Data outlet pin
-        
-        Usage:
-            PinBuilder.outlet(FLOAT, label='Result')
-            PinBuilder.outlet(FLOAT(label='Result'))
-        """
-        # If label provided as kwarg, override spec
-        if label:
-            spec = spec(label=label)
-        
-        return PinSpec(
-            flow_type=FlowType.DATA,
-            data_spec=spec,
-            label=spec.label,
-            is_outlet=True,
-            **kwargs
-        )
-    
-    @staticmethod
-    def ctrl_outlet(label: str = '', **kwargs) -> PinSpec:
-        return PinSpec(
-            flow_type=FlowType.CTRL,
-            label=label,
-            is_outlet=True,
-            **kwargs
-        )
